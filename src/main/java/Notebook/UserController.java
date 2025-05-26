ce6826d5d60806ce05fd7fb3cc8b4d1351c0febc
HryshkoDanylo <danylo.hryshko.pm.2022@lpnu.ua>
2025-05-26 07:33:06 +0300

    security

--- src/main/java/Notebook/UserController.java
+++ src/main/java/Notebook/UserController.java
@@ -2,6 +2,7 @@ package Notebook;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
@@ -24,8 +28,10 @@ public class UserController {
             model.addAttribute("error", "Користувач з таким іменем вже існує.");
             return "registration";
         }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         UserModel savedUser = userRepository.save(user);
-        session.setAttribute("user", savedUser);  // Автоматичний логін
+        session.setAttribute("user", savedUser);
         return "redirect:/notes";
     }
 
@@ -37,7 +43,8 @@ public class UserController {
     @PostMapping("/login")
     public String login(@RequestParam String username, @RequestParam String password, HttpSession session, Model model) {
         UserModel user = userRepository.findByUsername(username);
-        if (user != null && user.getPassword().equals(password)) {
+
+        if (user != null && passwordEncoder.matches(password, user.getPassword())) {
             session.setAttribute("user", user);
             return "redirect:/notes";
         } else {
